c80f2bde45e464cc78988b066f09948f639a9158
Vimal-Mudalagi <vimalmudalagi123@gmail.com>
2026-04-21 11:39:19 +0530

    Week 1: Packet classification + logger + dashboard

---
+++ issue/crypto_vimal/verifier_logger.py
@@ -0,0 +1,33 @@
+import logging
+from crypto_vimal.result_classifier import PacketStatus
+
+# Setup logger
+logger = logging.getLogger("VerifierLogger")
+logger.setLevel(logging.DEBUG)
+
+# File handler
+file_handler = logging.FileHandler("verifier.log")
+file_handler.setLevel(logging.DEBUG)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Formatter
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+
+def log_result(result):
+    if result.status == PacketStatus.VALID:
+        logger.info(f"{result.packet_id} - {result.status.value} - {result.reason}")
+
+    elif result.status in [PacketStatus.TAMPERED, PacketStatus.DECRYPTION_ERROR]:
+        logger.error(f"{result.packet_id} - {result.status.value} - {result.reason}")
+
+    else:
+        logger.warning(f"{result.packet_id} - {result.status.value} - {result.reason}")
